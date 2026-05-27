aeeb0db0b68af22372182c8b2408fe400af6ca51
Eamon Burns <eamon.r.burns@gmail.com>
2026-05-27 12:00:01 -0700

    feat: add dev-shell script and init.lua

---
+++ scripts/dev_init.lua
@@ -0,0 +1,20 @@
+if vim.env.DEV_SHELL ~= 'battery.nvim' then
+  -- TODO: Calculate dev-shell script that should be run based
+  -- on platform (Linux/MacOS: dev-shell.sh, Windows: DevShell.ps1)
+  vim.notify(
+    '  Not in battery.nvim dev-shell, exiting dev_init.lua early.\n'
+      .. '  Source scripts/dev-shell.sh to enter dev-shell.',
+    vim.log.levels.ERROR
+  )
+  return
+end
+
+vim.pack.add({
+  'https://github.com/nvim-lua/plenary.nvim',
+})
+vim.cmd.packadd('battery.nvim')
+
+require('battery').setup({})
+
+BatteryStatus = require('battery').get_battery_status
+BatteryStatusLine = require('battery').get_status_line
